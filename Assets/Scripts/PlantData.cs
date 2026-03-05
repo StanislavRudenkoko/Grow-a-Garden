26d7f3e60b4ad44991230eacc1c00cd965f58643
Fossa88 <zedey700@gmail.com>
2026-03-05 03:20:22 -0800

    Create PlantData.cs
    Holds data between the Garden menu and the Plant Manager scenes

---
+++ Assets/Scripts/PlantData.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+
+//<summary>
+// Hold data between scenes to show real time sprites in the garden scene
+// Authors: Joshua Trepanier
+// Date/revisions: March 4th 2026
+//</summary>
+public class PlantData : MonoBehaviour
+{
+    public static PlantData Instance;
+
+    public int currentStage;
+    public bool hasSoil;
+    public bool hasSeed;
+
+    //<summary>
+    // Singleton disign pattern to make sure only one PlantData object is made
+    // as these will keep be duplicated as you save plant data
+    //</summary>
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
